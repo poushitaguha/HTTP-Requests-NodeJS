e3b5789becebc903b8893ba534d091b15f3c21d2
poushitaguha <pguha.kolkata@gmail.com>
2019-06-03 16:27:21 +0000

    http printing in chunks

---
+++ step1-chunks.js
@@ -0,0 +1,30 @@
+function getAndPrintHTMLChunks () {
+
+var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+    console.log('Data', data, '\n');
+  });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
+
+console.log(getAndPrintHTMLChunks());
